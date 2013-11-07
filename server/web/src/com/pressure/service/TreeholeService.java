72cb83953a146bd1f1935fbfb9c3ab65ea92d53a
yunshang734 <yunshang_734@163.com>
2013-11-08 01:32:27 +0800

    TestSetTreehole

---
+++ server/web/src/com/pressure/service/TreeholeService.java
@@ -0,0 +1,37 @@
+package com.pressure.service;
+
+/**
+ * 
+ * @ClassName: TreeholeService
+ * @Description: TODO
+ * @author yunshang_734@163.com
+ * @date 2013-11-7 下午10:53:40
+ */
+public interface TreeholeService {
+	/**
+	 * 添加树洞
+	 * 
+	 * @param userId
+	 * @param pictureUrl
+	 * @param voice
+	 * @param location
+	 * @param content
+	 * @return
+	 */
+	public boolean addTreehole(long userId, String pictureUrl, String voice,
+			String location, String content);
+
+	/**
+	 * 修改树洞
+	 * 
+	 * @param id
+	 * @param userId
+	 * @param pictureUrl
+	 * @param voice
+	 * @param location
+	 * @param content
+	 * @return
+	 */
+	public boolean updateTreehole(long id, String pictureUrl, String voice,
+			String location, String content);
+}
